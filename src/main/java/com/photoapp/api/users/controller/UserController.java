78b92d2490e2e9950f5a2ad546d6077f96500ca9
Oleg Bezkorovaynyi <oleg.bezkorovajniy@gmail.com>
2021-06-08 15:40:54 +0300

    add setup for replication

--- src/main/java/com/photoapp/api/users/controller/UserController.java
+++ src/main/java/com/photoapp/api/users/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.photoapp.api.users.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +10,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
 
+    private final Environment environment;
+
+    @Autowired
+    public UserController(Environment environment) {
+        this.environment = environment;
+    }
+
     @GetMapping("/status/check")
     public String status() {
-        return "Working";
+        return "Working on the port: " + environment.getProperty("local.server.port");
     }
 }
