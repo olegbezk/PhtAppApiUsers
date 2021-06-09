bf4e9711474d89cc2f52da600d34e8cd38d7b21a
Oleg Bezkorovaynyi <oleg.bezkorovajniy@gmail.com>
2021-06-09 17:12:29 +0300

    add configuration server properties

--- src/main/java/com/photoapp/api/users/config/WebSecurity.java
+++ src/main/java/com/photoapp/api/users/config/WebSecurity.java
@@ -35,6 +35,7 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
+        // http.authorizeRequests().antMatchers("/**").hasIpAddress(env.getProperty("gateway.ip"))
         http.authorizeRequests().antMatchers("/users/**").permitAll()
                 .and()
                 .addFilter(getAuthenticationFilter());
